7867f6ef5f75a9d845450317858fe11642e83d09
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-22 14:11:25 +0300

    KS-1-5 Расчёт времени в космосе

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,2 +1,19 @@
 package lesson_1
 
+fun main() {
+
+    val timeInSpace = 6480
+    val hour = timeInSpace / 3600
+    val minute = timeInSpace / 60 % 60
+    val second = timeInSpace % 60
+
+    if (hour in 0..9) print("0$hour:")
+    else print("$hour:")
+
+    if (minute in 0..9) print("0$minute:")
+    else print("$minute:")
+
+    if (second in 0..9) print("0$second")
+    else print(second)
+
+}
