b9344aeb80a0e2c842ca29d4282c6f7395c7c608
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-22 14:04:13 +0300

    KS-1-2 Добавлено количество сотрудников

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,2 +1,15 @@
 package lesson_1
 
+fun main() {
+    val orderQuantity: Int = 75
+    val purchaseGratitude: String = "Спасибо за покупку!"
+    var numberOfStaff = 2000
+
+    println(orderQuantity)
+    println(purchaseGratitude)
+//    println(numberOfStaff)
+
+    numberOfStaff--
+
+    println(numberOfStaff)
+}
