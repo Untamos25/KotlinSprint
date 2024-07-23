85fab0008091b3b4d175a9200e844d521f26b952
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-23 13:27:07 +0300

    KS-2-5 Расчёт размера вклада

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,17 @@
+package lesson_2
+
+import kotlin.math.pow
+
+const val MAX_PERCENT = 100
+
+fun main() {
+    val initialAmount = 70000
+    val interestRate = 16.7
+    val depositPeriod = 20
+    val numberOfAccrualsPerYear = 1
+
+    val finalAmount = initialAmount * (1 + interestRate / MAX_PERCENT / numberOfAccrualsPerYear).pow(numberOfAccrualsPerYear * depositPeriod)
+
+    println(String.format("%.03f", finalAmount))
+
+}
