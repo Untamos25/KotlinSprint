de6def36b75386d5e2c3dcfbc7006cb3b61c7253
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-23 15:27:24 +0300

    KS-3-2 Вывод данных о гражданине

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    var lastName = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$lastName $firstName $patronymic, возраст $age")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $firstName $patronymic, возраст $age")
+}
