b63db02834e6c2095a09125e5f62cbc80b14a49e
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-23 10:46:38 +0300

    KS-1-5 Объявил константы на верхнем уровне

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,14 @@
 package lesson_1
 
+    const val SECONDS_IN_MINUTE = 60
+    const val MINUTES_IN_HOUR = 60
+
 fun main() {
-    val secondsInMinute = 60
-    val minutesInHour = 60
     val secondsInSpace = 6480
 
-    val showHours = secondsInSpace / (secondsInMinute * minutesInHour)
-    val showMinutes = secondsInSpace / secondsInMinute % minutesInHour
-    val showSeconds = secondsInSpace % secondsInMinute
+    val showHours = secondsInSpace / (SECONDS_IN_MINUTE * MINUTES_IN_HOUR)
+    val showMinutes = secondsInSpace / SECONDS_IN_MINUTE % MINUTES_IN_HOUR
+    val showSeconds = secondsInSpace % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", showHours, showMinutes, showSeconds))
 }
