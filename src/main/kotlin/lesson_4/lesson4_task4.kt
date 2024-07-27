4b493e431ff40e99a05d5aaa88b1e6020e9e162c
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-27 12:45:40 +0300

    KS-4-4 Объединил 4 переменные в 1 список

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,24 +3,18 @@ package lesson_4
 fun main() {
 
     val dayCounter = 5
-    var isDayHands = true
-    var isDayLegs = false
-    var isDayBack = false
-    var isDayAbs = true
 
-    if ((dayCounter + 1) % 2 == 1) {
-        isDayHands = false
-        isDayLegs = true
-        isDayBack = true
-        isDayAbs = false
-    }
+    val workout = if ((dayCounter + 1) % 2 == 1)
+        listOf(false, true, true, false) // Руки, ноги, спина, пресс
+    else
+        listOf(true, false, false, true) // Руки, ноги, спина, пресс
 
     println(
         """
-        Упражнения для рук:    $isDayHands
-        Упражнения для ног:    $isDayLegs
-        Упражнения для спины:  $isDayBack
-        Упражнения для пресса: $isDayAbs
+        Упражнения для рук:    ${workout[0]}
+        Упражнения для ног:    ${workout[1]}
+        Упражнения для спины:  ${workout[2]}
+        Упражнения для пресса: ${workout[3]}
     """.trimIndent()
     )
 }
