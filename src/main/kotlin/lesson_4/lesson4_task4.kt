de2960e615ad580d9557c210ccd23db84ca8856a
pavlushinsa <pavlushinsa18@gmail.com>
2024-07-27 22:13:43 +0300

    KS-4-4 Сократил количество переменных до одной с логическим типом

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,18 +3,14 @@ package lesson_4
 fun main() {
 
     val dayCounter = 5
-
-    val workout = if ((dayCounter + 1) % 2 == 1)
-        listOf(false, true) // Руки и пресс, ноги и спина
-    else
-        listOf(true, false) // Руки и пресс, ноги и спина
+    val whatTrainingToday = if ((dayCounter + 1) % 2 == 1) true else false // false = руки и пресс / true = ноги и спина
 
     println(
         """
-        Упражнения для рук:    ${workout[0]}
-        Упражнения для ног:    ${workout[1]}
-        Упражнения для спины:  ${workout[1]}
-        Упражнения для пресса: ${workout[0]}
+        Упражнения для рук:    ${!whatTrainingToday}
+        Упражнения для ног:    $whatTrainingToday
+        Упражнения для спины:  $whatTrainingToday
+        Упражнения для пресса: ${!whatTrainingToday}
     """.trimIndent()
     )
 }
