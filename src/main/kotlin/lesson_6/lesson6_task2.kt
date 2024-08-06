022aa611607e2e9c5988f982bfc7b04b8b402762
pavlushinsa <pavlushinsa18@gmail.com>
2024-08-06 15:48:54 +0300

    KS-6-2 Создание таймера

---
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -0,0 +1,19 @@
+package lesson_6
+
+fun main() {
+
+    print("Введите количество секунд для таймера: ")
+    var input: String? = readln()
+    while (input == null || !input.matches(Regex("\\d+"))) {
+        print("Недопустимое значение!\nВам нужно ввести целое число. Попробуйте ещё раз.\nВведите количество секунд: ")
+        input = readln()
+    }
+    var counter = input.toInt()
+
+    while (counter > 0) {
+        counter--
+        Thread.sleep(1000)
+    }
+    println("Прошло $input сек.")
+
+}
