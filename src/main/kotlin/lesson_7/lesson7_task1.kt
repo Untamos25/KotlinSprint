7c02fd6fc724309ff573c490fe3877ba87ec9979
pavlushinsa <pavlushinsa18@gmail.com>
2024-08-08 19:46:30 +0300

    KS-7-1 Из цикла вынесены интервалы, добавлена переменная для пароля

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -3,10 +3,11 @@ package lesson_7
 fun main() {
 
     val passLength = 6
+    var pass = ""
+    val chars = ('a'..'z')
+    val numbers = (0..9)
 
-    for (i in 1..passLength) {
-        val pass = if ((i + 1) % 2 == 0) ('a'..'z').random() else (0..9).random()
-        print(pass)
-    }
+    for (i in 1..passLength) pass += if ((i + 1) % 2 == 0) chars.random() else numbers.random()
+    println(pass)
 
 }
