331a1433c490d89c3a49b766bf9e4f4e4f7526e8
pavlushinsa <pavlushinsa18@gmail.com>
2024-08-12 13:09:42 +0300

    KS-10-1 Настроил вывод результатов функции в отлельные переменные

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,31 +1,28 @@
 package lesson_10
 
 fun main() {
-    playGame()
-}
-
-private fun rollTheDice() = (1..6).random()
-
-private fun playGame() {
-    print("\nХод игрока.\nНажмите Enter, чтобы бросить кости")
-    readln()
-    val diceUser = rollTheDice()
-    println("Очки игрока: $diceUser")
-    Thread.sleep(1500)
-
-    println("\nХод оппонента.")
-    val diceComp = rollTheDice()
-    println("Очки оппонента: $diceComp")
-    Thread.sleep(1500)
+    var turn = "Ход игрока."
+    val diceUser = rollTheDice(turn)
+    turn = "\nХод оппонента."
+    val diceComp = rollTheDice(turn)
 
     when {
         diceUser > diceComp -> println("\nВы выиграли!")
         diceUser < diceComp -> println("\nВы проиграли.")
-        else -> {
-            println("\nНичья! Игра начинается заново.")
-            playGame()
-        }
+        else -> println("\nНичья!")
     }
 }
 
+private fun generateDiceValue() = (1..6).random()
 
+private fun rollTheDice(turn: String): Int {
+    println(turn)
+    if (turn == "Ход игрока.") {
+        print("Нажмите Enter, чтобы бросить кости")
+        readln()
+    }
+    val dice = generateDiceValue()
+    println("Очков на костях: $dice")
+    Thread.sleep(1500)
+    return dice
+}
