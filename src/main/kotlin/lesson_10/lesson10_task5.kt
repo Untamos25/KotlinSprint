62e5f88d0d1dd1e80b41353aa297aca1c3ccbe6d
pavlushinsa <pavlushinsa18@gmail.com>
2024-08-13 13:46:29 +0300

    KS-10-5 исправлена логика в main(), getCart() теперь принимает ненулябельный токен

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -4,24 +4,22 @@ private const val VALID_LOGIN = "login"
 private const val VALID_PASSWORD = "pass"
 private const val TOKEN_LENGTH = 32
 
-
 fun main() {
     print("Введите логин: ")
     val inputLogin = readln()
     print("Введите пароль: ")
     val inputPassword = readln()
 
-    val cart = getCart(authorizeUser(inputLogin, inputPassword))
-
-    if (cart != null) {
+    val token = authorizeUser(inputLogin, inputPassword)
+    if (token != null) {
+        val cart = getCart(token)
         println("Корзина:")
         cart.forEach { println(" - $it") }
     } else println("Логин или пароль введены неверно.")
 }
 
-
-private fun authorizeUser(login: String, pass: String): String? {
-    return if (login == VALID_LOGIN && pass == VALID_PASSWORD) generateToken() else null
+private fun authorizeUser(login: String, password: String): String? {
+    return if (login == VALID_LOGIN && password == VALID_PASSWORD) generateToken() else null
 }
 
 private fun generateToken(): String {
@@ -31,7 +29,7 @@ private fun generateToken(): String {
     return token
 }
 
-private fun getCart(token: String?): List<String>? {
-    val cart = listOf("Товар 1", "Товар 2", "Товар 3")
-    return if (token != null) cart else null
+private fun getCart(token: String): List<String> {
+    println("Получен токен: $token\n")
+    return listOf("Товар 1", "Товар 2", "Товар 3")
 }
