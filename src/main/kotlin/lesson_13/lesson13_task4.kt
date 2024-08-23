4f1e6b6dc4c211aab1fdbfc8685ab094bbaebdbc
Stanislav <pavlushinsa18@gmail.com>
2024-08-23 17:49:50 +0300

    KS-13-4 Перенесена логика метода addContact из класса в тело основной функции в цикл for

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -5,24 +5,6 @@ class InfoContact(
     var telephoneNumber: Long? = null,
     var company: String? = null,
 ) {
-    fun addContact() {
-        println("Введите имя контакта:")
-        name = readln()
-
-        println("Введите номер телефона:")
-        telephoneNumber = readln().toLongOrNull()
-        if (telephoneNumber == null) {
-            println("Вы не ввели номер телефона. Этот контакт не будет добавлен.\n")
-            return
-        }
-
-        println("Введите название компании:")
-        company = readln()
-        if (company == "") company = null
-
-        println()
-    }
-
     fun printContact() {
         println("Имя: $name\nНомер телефона: $telephoneNumber\nКомпания: ${company ?: "<не указано>"}\n")
     }
@@ -36,9 +18,24 @@ fun main() {
 
     val telephoneBook = mutableListOf<InfoContact>()
     for (i in 0..<counter) {
-        val contact = InfoContact()
-        contact.addContact()
-        if (contact.telephoneNumber != null) telephoneBook.add(contact)
+        println("Введите имя контакта:")
+        val name = readln()
+
+        println("Введите номер телефона:")
+        val telephoneNumber = readln().toLongOrNull()
+        if (telephoneNumber == null) {
+            println("Вы не ввели номер телефона. Этот контакт не будет добавлен.\n")
+            continue
+        }
+
+        println("Введите название компании:")
+        var company: String? = readln()
+        if (company == "") company = null
+
+        println()
+
+        val contact = InfoContact(name, telephoneNumber, company)
+        telephoneBook.add(contact)
     }
 
     if (telephoneBook.isNotEmpty()) {
