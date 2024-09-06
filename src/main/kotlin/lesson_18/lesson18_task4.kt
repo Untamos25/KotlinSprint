31ec4ed2786f3f72246435bd3827077b258e9bc1
Stanislav <pavlushinsa18@gmail.com>
2024-09-06 17:55:41 +0300

    KS-18-4 Интерфейс заменён на абстрактный класс, функции теперь только возвращают значения

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -2,30 +2,25 @@ package lesson_18
 
 import kotlin.math.pow
 
-interface Parcel {
-    fun getParcelSurfaceArea()
+abstract class Parcel {
+    abstract fun getParcelSurfaceArea(): Double
 }
 
-class RectangularParcel(val length: Double, val width: Double, val height: Double) : Parcel {
-    override fun getParcelSurfaceArea() {
-        val area = (length * width + width * height + length * height) * 2
-        println("Площадь прямоугольной посылки = $area")
-    }
+class RectangularParcel(val length: Double, val width: Double, val height: Double) : Parcel() {
+    override fun getParcelSurfaceArea() = (length * width + width * height + length * height) * 2
 }
 
-class CubeParcel (private val length: Double) : Parcel {
-    override fun getParcelSurfaceArea() {
-        val area = length.pow(2) * 6
-        println("Площадь квадратной посылки = $area")
-    }
+class CubeParcel(private val length: Double) : Parcel() {
+    override fun getParcelSurfaceArea() = length.pow(2) * 6
 }
+
 
 fun main() {
     val rectangularParcel = RectangularParcel(4.0, 3.0, 2.0)
     val cubeParcel = CubeParcel(3.0)
 
-    val listOfParcels : List<Parcel> = listOf(rectangularParcel, cubeParcel)
+    val listOfParcels: List<Parcel> = listOf(rectangularParcel, cubeParcel)
     listOfParcels.forEach {
-        it.getParcelSurfaceArea()
+        println("Площадь поверхности посылки: ${it.getParcelSurfaceArea()}")
     }
 }
