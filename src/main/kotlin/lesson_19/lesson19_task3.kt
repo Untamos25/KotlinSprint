4b3b289d073b8378359055bfb7dc73de22a69d7c
Stanislav <pavlushinsa18@gmail.com>
2024-09-08 15:46:18 +0300

    KS-19-3 Демонстрация TODO() и NotImplementedError

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,16 @@
+package lesson_19
+
+class Spaceship {
+
+    fun takeOff() {
+        TODO("Нужна дополнительная логика")
+    }
+
+    fun land() {
+        println("Приземлиться")
+    }
+
+    fun shootBackAtAsteroid() {
+        throw NotImplementedError("Эта функция ещё не реализована")
+    }
+}
