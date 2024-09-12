bc5f3ae327b156f87490bd449c828314f9f1482e
Stanislav <pavlushinsa18@gmail.com>
2024-09-12 17:02:46 +0300

    KS-20-4 исправлен список listOfLambdas: теперь он является списком лямбд, а не строк

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -3,9 +3,10 @@ package lesson_20
 fun main() {
     val listOfElements = listOf("Кнопка 1", "Кнопка 2", "Кнопка 3", "Кнопка 4", "Кнопка 5", "Кнопка 6")
 
-    val mapOfLambdas = listOfElements.map { element -> { it: String -> "Нажат элемент $it" }(element) }
+    val listOfLambdas = listOfElements.map { _ -> { element: String -> "Нажат элемент $element" } }
 
-    mapOfLambdas.forEachIndexed { index, lambda ->
-        if ((index + 1) % 2 == 0) println(lambda)
+    listOfLambdas.forEachIndexed { index, lambda ->
+        if ((index + 1) % 2 == 0)
+            println(lambda(listOfElements[index]))
     }
 }
