483b6d7be997cf87643b62c7d774bc594f84d28a
Stanislav <pavlushinsa18@gmail.com>
2024-09-13 15:49:08 +0300

    KS-20-4 Лямбда теперь получает индекс элемента при её создании, что позволяет вызывать её без параметра

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -3,10 +3,11 @@ package lesson_20
 fun main() {
     val listOfElements = listOf("Кнопка 1", "Кнопка 2", "Кнопка 3", "Кнопка 4", "Кнопка 5", "Кнопка 6")
 
-    val listOfLambdas = listOfElements.map { _ -> { element: String -> "Нажат элемент $element" } }
+    val listOfLambdas = listOfElements.mapIndexed { _, element ->
+        { "Нажат элемент $element" }
+    }
 
     listOfLambdas.forEachIndexed { index, lambda ->
-        if ((index + 1) % 2 == 0)
-            println(lambda(listOfElements[index]))
+        if ((index + 1) % 2 == 0) println(lambda())
     }
 }
