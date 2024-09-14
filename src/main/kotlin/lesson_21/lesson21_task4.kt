331423288032feb3530573102e53d7d52ad0235d
Stanislav <pavlushinsa18@gmail.com>
2024-09-14 14:03:25 +0300

    KS-21-4 уделена лишняя переменная, сокращён код функции writeWordToStart

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -3,15 +3,12 @@ package lesson_21
 import java.io.File
 
 fun File.writeWordToStart(word : String) {
-    val lowerCaseWord = word.lowercase()
     val fileContent = this.readText()
-
-    this.writeText("")
-    this.appendText("$lowerCaseWord\n$fileContent")
+    this.writeText("${word.lowercase()}\n$fileContent")
 }
 
 fun main() {
-    val word = "First"
+    val word = "New word"
     val file = File("textFile.txt")
 
     file.writeWordToStart(word)
