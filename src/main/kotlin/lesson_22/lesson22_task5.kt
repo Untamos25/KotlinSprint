d792d33f277d2312518676b30aefb6b9d59a1559
Stanislav <pavlushinsa18@gmail.com>
2024-09-15 13:07:16 +0300

    KS-22-5 Создание класса данных для хранения информации о местах и событиях в голактике

---
+++ src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -0,0 +1,28 @@
+package lesson_22
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val dateTime: LocalDateTime,
+    val distanceFromEarth: Double
+)
+
+fun main() {
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")
+
+    val alphaCentauri = GalacticGuide(
+        name = "Альфа Центавра",
+        description = "Ближайшая к Солнечной системе звездная система, " +
+                "состоящая из трех звезд: Альфа Центавра A, Альфа Центавра B и Проксима Центавра.",
+        dateTime = LocalDateTime.of(1997, 9, 1, 9, 18),
+        distanceFromEarth = 4.36
+    )
+
+    println("Название: ${alphaCentauri.name}")
+    println("Описание: ${alphaCentauri.description}")
+    println("Земная дата и время события: ${alphaCentauri.dateTime.format(formatter)}")
+    println("Расстояние от Земли: ${alphaCentauri.distanceFromEarth} световых лет")
+}
